e35db8dde27226ee22b24e0441f09c83d3d1359d
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-12-13 13:33:06 -0300

    Separate vue filter in there own module

---
+++ src/filters.js
@@ -0,0 +1,41 @@
+import dateFns from 'date-fns'
+import accounting from './lib/accounting'
+
+const currencyOptions = {
+  symbol: '$',
+  decimal: ',',
+  thousand: '.',
+  precision: 2,
+  format: {
+    pos: '%s %v',
+    neg: '%s (%v)',
+    zero: '-',
+  }
+}
+
+const numberOptions = {
+  decimal: ',',
+  thousand: '.',
+  precision: 2,
+}
+
+export default function filters(Vue) {
+  if (filters.installed) {
+    return
+  }
+
+  Vue.filter('date', value => {
+    if (value && value.$date) return dateFns.format(value.$date, 'D/MM/YYYY')
+    return value
+  })
+
+  Vue.filter('money', value => {
+    if (!isNaN(value)) return accounting.formatMoney(value, currencyOptions)
+    return value
+  })
+
+  Vue.filter('number', value => {
+    if (!isNaN(value)) return accounting.formatNumber(value, numberOptions)
+    return value
+  })
+}
--- src/main.js
+++ src/main.js
@@ -3,20 +3,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Resource from 'vue-resource'
-import Potion from 'vue-potion'
+// import Potion from 'vue-potion'
 import Material from 'vue-material'
 import 'vue-material/dist/vue-material.css'
-import dateFns from 'date-fns'
-import accounting from './lib/accounting'
 
 import Application from './Application'
 import routes from './routes'
 import themes from './themes'
 
+import Filters from './filters'
+
 Vue.use(Router)
 Vue.use(Resource)
-Vue.use(Potion)
+// Vue.use(Potion)
 Vue.use(Material)
+
+Vue.use(Filters)
 
 Vue.http.options.root = 'http://localhost:5000/api'
 
@@ -24,39 +26,6 @@ Vue.material.theme.registerAll(themes)
 
 const router = new Router({
   routes
-})
-
-Vue.filter('date', value => {
-  if (value && value.$date) return dateFns.format(value.$date, 'D/MM/YYYY')
-  return value
-})
-
-const currencyOptions = {
-  symbol: '$',
-  decimal: ',',
-  thousand: '.',
-  precision: 2,
-  format: {
-    pos: '%s %v',
-    neg: '%s (%v)',
-    zero: '-',
-  }
-}
-
-const numberOptions = {
-  decimal: ',',
-  thousand: '.',
-  precision: 2,
-}
-
-Vue.filter('money', value => {
-  if (!isNaN(value)) return accounting.formatMoney(value, currencyOptions)
-  return value
-})
-
-Vue.filter('number', value => {
-  if (!isNaN(value)) return accounting.formatNumber(value, numberOptions)
-  return value
 })
 
 /* eslint-disable no-new */
