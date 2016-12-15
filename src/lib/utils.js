c35d3d65ee2bd694fafdf52efc391a9ea42d0aa7
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-12-15 18:21:16 -0300

    Minor fix on utils.paddingLeft

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,4 @@
 
 export function paddingLeft(padding, value) {
-  return String(padding + value).slice(-padding.length);
+  return String(padding + value).slice(-Math.max(padding.length, value.length));
 }
