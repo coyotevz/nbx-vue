64fac4ca873eb95963ee9f16d6626ccf4ea1e391
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-12-30 04:52:34 -0300

    Start working with vuex

--- src/components/supplier/List.vue
+++ src/components/supplier/List.vue
@@ -5,7 +5,7 @@
         <div class="ui-list-cell header-title">Proveedores por nombre</div>
       </div>
     </div>
-    <div class="ui-list-body" role="listbox" v-if="suppliers.length">
+    <div class="ui-list-body" role="listbox" v-if="suppliers && suppliers.length">
       <div class="ui-list-item" v-for="supplier in suppliers"
         @click="showDetail(supplier.$id)">
         <div class="ui-list-cell item-icon"><md-icon>business</md-icon></div>
@@ -37,57 +37,16 @@ export default {
     return {
       loading: false,
       error: null,
-      suppliers: [],
-      tableSort: {
-        name: 'rz',
-        type: 'asc',
-      },
-      tableOptions: {
-        total: 0,
-        page: 1,
-        size: 10,
-      },
     }
   },
   created() {
     this.$root.setTitle('Proveedores')
-    this.fetchData()
+    this.$store.dispatch('fetchSuppliers')
   },
-  watch: {
-    'tableSort': 'fetchData',
+  computed: {
+    suppliers: state => state.$store.suppliers
   },
   methods: {
-    onSort(ev) {
-      this.tableSort.name = ev.name
-      this.tableSort.type = ev.type
-      this.fetchData()
-    },
-    onPagination(ev) {
-      this.tableOptions.size = ev.size
-      this.tableOptions.page = ev.page
-      this.fetchData()
-    },
-    fetchData() {
-      this.loading = true
-      let sort = {}
-      sort[this.tableSort.name] = this.tableSort.type === 'desc'
-
-      const params = {
-        sort: JSON.stringify(sort),
-        per_page: this.tableOptions.size,
-        page: this.tableOptions.page,
-      }
-
-      this.$http.get('suppliers', { params: params }).then(response => {
-        this.loading = false
-        this.error = null
-        this.suppliers = response.data
-        this.tableOptions.total = parseInt(response.headers['x-total-count'], 10)
-      }).catch(error => {
-        this.loading = false
-        this.error = error
-      })
-    },
     showDetail(id) {
       this.$router.push({ name: 'suppliers:detail', params: { 'id': id } })
     }
@@ -106,35 +65,4 @@ export default {
     color: #4285f4;
   }
 }
-
-/*
-.suppliers-list {
-  justify-content: space-between;
-
-  .md-table-header,
-  .md-table-body {
-    background-color: #fff;
-  }
-
-  .no-content-table {
-    text-align: center;
-  }
-
-  .md-table-pagination {
-    // flex: 0;
-  }
-
-  .spinner-container {
-    position: absolute;
-    top: 56px;
-    bottom: 56px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 1;
-    background-color: rgba(255, 255, 255, 0.8);
-  }
-}
-*/
 </style>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from './api'
 
 Vue.use(Vuex)
+
+const RECEIVE_SUPPLIERS = 'RECEIVE_SUPPLIERS'
 
 export default new Vuex.Store({
   state: {
@@ -10,13 +13,16 @@ export default new Vuex.Store({
   getters: {
     suppliers: state => state.suppliers
   },
-  mutators: {
-    receiveSuppliers(state, { suppliers }) {
+  mutations: {
+    [RECEIVE_SUPPLIERS](state, { suppliers }) {
       state.suppliers = suppliers
     }
   },
   actions: {
     fetchSuppliers({ commit }) {
+      api.getSuppliers(suppliers => {
+        commit(RECEIVE_SUPPLIERS, { suppliers })
+      })
     }
   }
 })
