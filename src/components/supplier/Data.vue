48d3a80fc824761fc264f342263bc35fb1b7f481
Augusto Roccasalva <augusto@rioplomo.com.ar>
2017-01-03 13:42:16 -0300

    Styling data cards

--- src/components/supplier/Data.vue
+++ src/components/supplier/Data.vue
@@ -5,7 +5,7 @@
         <md-card>
           <md-card-header>
             <div class="md-title">Datos Fiscales</div>
-            <md-button class="md-icon-button edit"><md-icon>edit</md-icon></md-button>
+            <md-button class="md-icon-button edit md-dense"><md-icon>edit</md-icon></md-button>
           </md-card-header>
 
           <md-card-content>
@@ -22,24 +22,7 @@
         <md-card>
           <md-card-header>
             <div class="md-title">Datos Fiscales</div>
-            <md-button class="md-icon-button edit"><md-icon>edit</md-icon></md-button>
-          </md-card-header>
-
-          <md-card-content>
-            Lorem corrupti corrupti quaerat labore quo Natus libero ipsa nostrum ad perspiciatis Quibusdam vel iusto dolor fugiat eos? Sint reiciendis animi unde perferendis libero voluptatibus? Veritatis nisi tempore adipisci facilis?
-          </md-card-content>
-
-          <md-card-actions>
-            <md-button class="md-primary">Action</md-button>
-            <md-button>Action</md-button>
-          </md-card-actions>
-        </md-card>
-      </md-layout>
-      <md-layout>
-        <md-card>
-          <md-card-header>
-            <div class="md-title">Datos Fiscales</div>
-            <md-button class="md-icon-button edit"><md-icon>edit</md-icon></md-button>
+            <md-button class="md-icon-button edit md-dense"><md-icon>edit</md-icon></md-button>
           </md-card-header>
 
           <md-card-content>
@@ -71,16 +54,28 @@ export default {
 .supplier-data {
   padding-top: 25px;
 
-  .md-card-header {
-    display: flex;
-    .md-title {
-      flex: 1;
-      font-size: 16px;
-      line-height: 20px;
-    }
-    .md-button.md-icon-button {
-      line-height: 20px;
-      padding: 0;
+  .md-card {
+    margin: 0 4px 16px;
+
+    .md-card-header {
+      display: flex;
+      .md-title {
+        flex: 1;
+        font-size: 16px;
+        line-height: 20px;
+        margin-top: 0 !important;
+      }
+      .md-button.md-icon-button {
+        line-height: 20px;
+        margin-top: -8px;
+        i {
+          font-size: 20px;
+          width: 20px;
+          min-width: 20px;
+          height: 20px;
+          min-height: 20px;
+        }
+      }
     }
   }
 }
